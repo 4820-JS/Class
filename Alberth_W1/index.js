6b2bf93d2ec9fe79ab9506347f98a859be046f08
ALFAROSO <87240930+ALFAROSO@users.noreply.github.com>
2023-05-24 21:54:45 -0700

    Add files via upload

---
+++ Alberth_W1/index.js
@@ -0,0 +1,26 @@
+console.log("farts")
+//alert("ok")
+//this is a Comment
+//Comments are notes to yourself that let you know what is going on in your code.
+//it is also handy to toggle code on and off
+//It will not show up in the console.
+/*
+
+more comments
+
+*/
+
+/*
+
+There are 3 data types in JavaScript:
+1. Numbers (0,1,2,3,4.....)
+2. Booleans (true or flase)
+3. Strings (sentences or characters in between quotation marks.)
+There are 3:
+    3.1 "double quotes"
+    3.2 'single quotes'
+    3.3 `back ticks`
+
+*/
+
+console.log(`I love to eat ${12+5} pieces of brocoli`)
