0f6254e5f92e34b42037646a35694bc82a07c933
ALFAROSO <87240930+ALFAROSO@users.noreply.github.com>
2023-06-06 15:47:42 -0700

    Update main.js

--- Alberth_W5/Alberth-W5-in-class-starter/js/main.js
+++ Alberth_W5/Alberth-W5-in-class-starter/js/main.js
@@ -1,1 +1,54 @@
 //This is a toDo list app, that saves the entries from a form in the UI (HTML)
+
+console.log('test')
+
+//ID names from html===============================================
+
+//Input box: userInput
+//button: addItem
+//output: outputArea
+
+
+//.value gets the value from an inputbox. but first we need an event
+//console.log(userInput.value)
+
+
+//EVENTS
+// addItem.addEventListener("click", function(){
+//     console.log(userInput.value)
+// })
+
+// addItem.addEventListener("click", function(){
+//     let value = userInput.value
+//     outputArea.innerHTML = value
+// })
+
+
+
+// addItem.addEventListener("click", function(){
+//     let value = userInput.value
+//     outputArea.innerHTML = `<h1>${value}</h1>`
+// })
+
+// addItem.addEventListener("click", function(){
+//     let value = userInput.value
+//     addItem.innerHTML = value
+// })
+
+let greetingArr = []
+
+addItem.addEventListener("click", function(){
+    
+    let resut = ""
+    let inputVal = userInput.value
+    //
+    greetingArr.push(inputVal)
+
+    for(let i = 0; i < greetingArr.length; i++){
+        resut += `<h1>${greetingArr[i]}</h1>`
+    }
+    outputArea.innerHTML = resut
+})
+//
+//
+//
